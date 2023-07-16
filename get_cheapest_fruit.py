46c0f98a578ac9bc830d529ad486da7200b5bd75
Mac <mac@MacBook-Air-Mac.local>
2023-07-16 16:11:04 +0500

    pro4

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -1,3 +1,7 @@
+from pprint import pprint
+import csv
+
+
 def get_cheapest_fruit(data: str) -> str:
     """
     This function returns the name of the cheapest fruit
@@ -8,4 +12,19 @@ def get_cheapest_fruit(data: str) -> str:
         str: name of the cheapest fruit
     """
     # your code here
-    pass
+    f = open(data, 'r')
+    reader=csv.reader(f)
+    fruits=list(reader)
+    most_expensive=fruits[1]
+    for fruit in fruits[2:]:
+        if float(fruit[1])<float(most_expensive[1]):
+            most_expensive=fruit
+
+    f.close()
+
+
+    return most_expensive[0] 
+data='fruits.csv'
+pprint(get_cheapest_fruit(data))
+
+
