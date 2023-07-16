f87de980f4133d6f249bb1411316ddcc2193440b
Mac <mac@MacBook-Air-Mac.local>
2023-07-16 16:04:35 +0500

    pro3

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+import csv
+
 def get_expensive_fruit(data: str) -> str:
     """
     This function returns the name of the most expensive fruit
@@ -8,6 +11,19 @@ def get_expensive_fruit(data: str) -> str:
         str: name of the most expensive fruit
     """
     # your code here
-    return  
+    f = open(data, 'r')
+    reader=csv.reader(f)
+    fruits=list(reader)
+    most_expensive=fruits[1]
+    for fruit in fruits[2:]:
+        if float(fruit[1])>float(most_expensive[1]):
+            most_expensive=fruit
+
+    f.close()
+
+
+    return most_expensive[0] 
+data='fruits.csv'
+pprint(get_expensive_fruit(data))
 
 
