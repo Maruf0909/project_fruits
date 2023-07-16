e64be4e0850f0d2a3f4b9d0344ea05be5ec554db
Mac <mac@MacBook-Air-Mac.local>
2023-07-16 15:14:44 +0500

    pro1

--- get_total_price.py
+++ get_total_price.py
@@ -1,3 +1,7 @@
+from pprint import pprint
+import csv
+
+
 def get_total_price(data:str)->float:
     """
     This function returns the total price of the fruits
@@ -7,6 +11,14 @@ def get_total_price(data:str)->float:
     returns:
         float: fruits total price
     """
-    return
+    total = 0
 
+    f = open(data, 'r')
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        total += float(row[1])
+    f.close()
+    return round(total, 2)
+pprint(get_total_price('fruits.csv'))
     
